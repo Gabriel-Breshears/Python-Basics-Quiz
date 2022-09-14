79b52e38bcddecf245ee23cf4d54e23401e170b8
Gabriel <74091198+Gabriel-Breshears@users.noreply.github.com>
2022-09-14 09:28:00 -0500

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,110 @@
+
+# 1. what is the output of this code?
+# output defintion: output is just what happens onces all the code is done,the end result. after all the calculation are done its what get put into the console. basically its what gets put out.
+print('hello world')
+
+
+
+# 2. What is the output of this code?
+# Hint: remember python divison is a bit different.
+x = 55 / 11 
+print(x)
+
+#3. What is the output of this code? 
+# Hint:Read the code carefully
+
+x = 1 
+
+if x > 0:
+  x = 0
+  print('Negative to zero')
+elif x == 0:
+  print('zero')
+elif x == 1:
+  print('one')
+else:
+  print('More')
+
+
+#4 What is the output of this code
+name = 'Finn'
+print(name + ' the human')
+
+
+
+#5 What is the output of this code?
+#This is a comment
+answer = 42 #the answer
+
+text = '# Is this a comment?'
+print(text)
+
+
+
+#6 What is the output of this code?
+engine_sound = 'vroom'
+print(engine_sound * 4)
+
+# 7 What is the output of this code?
+print(3 * 'un' + 'ium')
+
+
+# 8 What is the output of this code?
+
+print(1.0 * 10 - 1)
+
+#9 What is the output of this code?
+
+x = 1 
+
+if x < 0:
+  x = 0
+  print('Negative to zero')
+elif x == 0:
+  print('zero')
+elif x == 1:
+  print('one')
+else:
+  print('More')
+
+
+
+
+# 10 Find the bugs. tell me how many bugs there are and how to fix them. 
+the_karate_kid = "Johnny Lawrence"
+
+if the_karate_kid == "Johnny Lawrence"
+  print(karate_kid)
+
+
+# 11 How do we get input from the user? create a variable called name and ask the user "what is your name"
+
+
+#12 What is the bug? How do you fix it? 
+# Hint there are two data types at work here str and int. 
+
+favorite_number = input('What is your favorite number?')
+
+# user types in the number 9 
+
+if favorite_number == 1:
+  print('That is a good number')
+elif favorite_number == 2:
+  print('That is a good number')
+else:
+  print('That is a good number')
+
+
+
+
+  
+#Bonus! What is the output of this code
+# good recomdation aurora 
+
+print('''
+easy as 
+A
+B
+C
+'''
+)
